9100d7724c7a5f313eeff05d8bb584f64430439c
Fanzhi-Zhu <pkw86fxnmv@privaterelay.appleid.com>
2026-09-26 13:13:44 +0800

    fix: 修正目录层级，分成Fanzhi-Zhu嵌套task0文件夹

--- foundation/Fanzhi-Zhu/task0/洛谷P1001.py
+++
@@ -1,3 +0,0 @@
-data=input().split()
-sum=int(data[0])+int(data[1])
-print(sum)
--- foundation/Fanzhi-Zhu/task0/洛谷P1046.py
+++
@@ -1,7 +0,0 @@
-data=input().split()
-height=int(input())
-count=0
-for i in range(10) :
-    if int(data[i])<=height+30 :
-        count+=1
-print(count)
